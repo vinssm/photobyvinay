cb4cc409ca3099d4d404f25f9b715de18cec247d
Vinay <vallab.v@gmail.com>
2026-06-17 09:39:34 -0400

    confifure github deployment1

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
 // Connect without the deprecated configuration options object
-mongoose.connect();
+mongoose.connect(
+  "mongodb+srv://anunaidumeka_db_user:Gpy7KSwvgjCcotnp@cluster0.mbkdql3.mongodb.net/?appName=Cluster0",
+);
 
 module.exports = mongoose.connection;
